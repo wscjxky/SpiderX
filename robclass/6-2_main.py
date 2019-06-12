6bdd22489ad5ede964308191dd46f1a832773521
wscjxky <619677632@qq.com>
2019-06-13 01:09:15 +0800

    i

--- robclass/6-2_main.py
+++ robclass/6-2_main.py
@@ -126,57 +126,60 @@ def is_free(kecheng_code, xuhao, proxy='', pred_type='pp'):
             # is_free(kecheng_code, xuhao, proxy=proxy, pred_type=pred_type)
     soup = BeautifulSoup(res.text, 'html.parser')
     table = soup.find('div', id='container')
-    if table:
-        class_trs = table.find_all('tr')[1:]
-        for tr in class_trs:
-            for index_kecheng, k_code in enumerate(kecheng_code):
-                if k_code in tr.text:
-                    has_free = tr.find('input')
-                    if has_free:
-                        class_code = has_free["value"].strip()
-                        class_name = tr.find('div', class_='ellipsis')
-                        if class_name:
-                            class_name = class_name.text.strip()
-                        else:
-                            class_name = tr.find('div', class_='hide').text.strip()
-                        class_name = re.search("】(.*)", class_name).group(1)
-                        if xuhao[index_kecheng] in class_name:
-                            print("有课余量：")
-                            try:
-                                import winsound
-                                duration = 1000  # millisecond
-                                freq = 500  # Hz
-                                winsound.Beep(freq, duration)
-                            except:
-                                import os
-                                duration = 1  # second
-                                freq = 500  # Hz
-                                os.system(
-                                    'play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
-                            print(class_name)
-                            print(class_code)
-                            res = requests.get('https://dean.bjtu.edu.cn/captcha/refresh/', cookies=cookies,
-                                               headers=headers_image)
-                            json_data = res.json()
-                            hashkey = json_data['key']
-                            print(json_data)
-                            img_data = requests.get('https://dean.bjtu.edu.cn' + json_data['image_url'],
-                                                    headers=headers)
-                            answer, req_id = api.Predict(40300, img_data.content)
-                            print(answer)
-                            result = post_request(cookies=cookies, class_code=class_code, hashkey=hashkey,
-                                                  answer=answer,
-                                                  req_id=req_id, pred_type='cjy')
-
-                            if result == 403:
-                                answer, req_id = chaojiying.PostPic(img_data.content, 2003)
+    try:
+        if table:
+            class_trs = table.find_all('tr')[1:]
+            for tr in class_trs:
+                for index_kecheng, k_code in enumerate(kecheng_code):
+                    if k_code in tr.text:
+                        has_free = tr.find('input')
+                        if has_free:
+                            class_code = has_free["value"].strip()
+                            class_name = tr.find('div', class_='ellipsis')
+                            if class_name:
+                                class_name = class_name.text.strip()
+                            else:
+                                class_name = tr.find('div', class_='hide').text.strip()
+                            class_name = re.search("】(.*)", class_name).group(1)
+                            if xuhao[index_kecheng] in class_name:
+                                print("有课余量：")
+                                try:
+                                    import winsound
+                                    duration = 1000  # millisecond
+                                    freq = 500  # Hz
+                                    winsound.Beep(freq, duration)
+                                except:
+                                    import os
+                                    duration = 1  # second
+                                    freq = 500  # Hz
+                                    os.system(
+                                        'play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
+                                print(class_name)
+                                print(class_code)
+                                res = requests.get('https://dean.bjtu.edu.cn/captcha/refresh/', cookies=cookies,
+                                                   headers=headers_image)
+                                json_data = res.json()
+                                hashkey = json_data['key']
+                                print(json_data)
+                                img_data = requests.get('https://dean.bjtu.edu.cn' + json_data['image_url'],
+                                                        headers=headers)
+                                answer, req_id = api.Predict(40300, img_data.content)
                                 print(answer)
                                 result = post_request(cookies=cookies, class_code=class_code, hashkey=hashkey,
                                                       answer=answer,
-                                                      req_id=req_id, pred_type='pp')
+                                                      req_id=req_id, pred_type='cjy')
 
-                            if result == 200:
-                                return True
+                                if result == 403:
+                                    answer, req_id = chaojiying.PostPic(img_data.content, 2003)
+                                    print(answer)
+                                    result = post_request(cookies=cookies, class_code=class_code, hashkey=hashkey,
+                                                          answer=answer,
+                                                          req_id=req_id, pred_type='pp')
+
+                                if result == 200:
+                                    return True
+    except Exception as e:
+        print(e)
     return False
 
 
