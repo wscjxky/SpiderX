752b72ba88b6a9558da17bdc54d01a1170dd2bcf
wscjxky <619677632@qq.com>
2019-12-16 18:13:31 +0800

    12-16

--- robclass/fabfile.py
+++ robclass/fabfile.py
@@ -6,6 +6,12 @@ local_con = Context()
 def commit(message):
     local_con.run('git add -A && git commit -m ' + message, warn=True)
 
+def pull():
+    local_con.run('git pull', warn=True)
+
+
+def push():
+    local_con.run('git push', warn=True)
 @task
 def deploy(ctx, message=''):
     commit(message)
